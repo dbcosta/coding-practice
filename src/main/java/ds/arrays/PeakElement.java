c2b179efdc3ff0e6e4101e3208b2665dd4ed3a7a
belinda <belindadcosta124@gmail.com>
2022-04-19 20:11:10 +0530

    Day-1: Peak Element Challenge. More changes

--- src/main/java/ds/arrays/PeakElement.java
+++ src/main/java/ds/arrays/PeakElement.java
@@ -1,5 +1,7 @@
 package ds.arrays;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 /**
  * An element is called a peak element if its value is not smaller
  * than the value of its adjacent elements(if they exists).
@@ -13,24 +15,26 @@ public class PeakElement {
 
     private int findPeakElement(int arr[],int size){
         int index = 0;
+        int peakElement = 0;
         for (int i = 0; i < arr.length; i=i+2) {
             if(i==0){
                 //compare values at 0 and 1 position
-                compare(arr[i],arr[i+1]);
+                peakElement = compare(arr[i],arr[i+1]);
+
             }else if(i==arr.length-1){
                 //compare values at last and second-last position
-                compare(arr[i-1],arr[i]);
+                peakElement = compare(arr[i-1],arr[i]);
             }else{
                 //compare all positions.
-                compare(arr[i-1],arr[i],arr[i+1]);
+                peakElement = compare(arr[i-1],arr[i],arr[i+1]);
             }
-            index = i;
         }
-        return index;
+        System.out.println(peakElement);
+        return ArrayUtils.indexOf(arr,peakElement);
     }
 
-    private void compare(int a,int b){
-        Math.max(a,b);
+    private int compare(int a,int b){
+       return Math.max(a,b);
     }
 
     private int compare(int a,int b,int c){
@@ -45,7 +49,7 @@ public class PeakElement {
 
     public static void main(String[] args) {
         PeakElement peakElement = new PeakElement();
-        int arr[] = {11,2,3,4,10};
+        int arr[] = {11,2,3,4,0};
         System.out.println(peakElement.findPeakElement(arr,5));
     }
 }
